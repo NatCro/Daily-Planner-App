26a79fb5facc15550b644d7550bbd25db28eafe8
NatCro <njcrockwell@gmail.com>
2023-01-22 11:18:19 +0000

    added the current time to the top of the page. Adding psuedo code to hel structure what I need to do. Started work on the function to see what the current time is to change the colour accordingly

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@
 //need variable for current time
 
 
-$(dcument).ready(function(){
+$(document).ready(function(){
     $(".saveBtn").on("click", function(){
     //this gets nearby values of description class
     var value = $(this.siblings(".description")).val();
@@ -11,3 +11,20 @@ $(dcument).ready(function(){
     localStorage.setItem(time, value)
 })
 })
+
+//variable for current time to compare to each section
+
+// function to check for the current time and check the colour is correct (change the class to reflect the colour?)
+
+//save text to the box by logging to local storage
+
+//display current date/time at the top of the page
+var currentTime = moment().format("dddd MM YYYY HH:mm")
+document.getElementById("currentDay").innerHTML = currentTime
+
+
+//seeing what the time is to change the colour of the block
+var timeCompare = parseInt(moment().format("HH"))
+
+console.log(timeCompare)
+
